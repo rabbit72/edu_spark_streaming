b51682c0827e6c4063a53966b7012405239b09fc
Daniil Shadrin <rabbit72rus@gmail.com>
2019-02-18 19:52:43 +0300

    update cli

--- producer.py
+++ producer.py
@@ -13,14 +13,15 @@ def send_to_kafka(line):
 @click.argument('csv_file', type=click.File('rb'))
 @click.option('--topic', '-t', default='test')
 @click.option('--process_quantity', type=int, default=multiprocessing.cpu_count())
-@click.option('--host', type=str, default='172.18.0.2')
-@click.option('--port', type=int, default=6667)
-def main(csv_file, topic, process_quantity, host, port):
+@click.option('--host', '-h', type=str, default='172.18.0.2')
+@click.option('--port', '-p', type=int, default=6667)
+@click.option('--header-pass', '-head', type=bool, default=True)
+def main(csv_file, topic, process_quantity, host, port, header_pass):
 
     global KAFKA_TOPIC, KAFKA_ADDRESS
     KAFKA_TOPIC, KAFKA_ADDRESS = topic, f'{host}:{port}'
-
-    next(csv_file)  # skip header
+    if header_pass:
+        next(csv_file)  # skip header
     with multiprocessing.Pool(process_quantity) as p:
         p.map(send_to_kafka, csv_file)
 
