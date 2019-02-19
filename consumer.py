41d0392dd6e85f2d87861cb4a83980b1480b72b1
Daniil Shadrin <rabbit72rus@gmail.com>
2019-02-19 20:31:59 +0300

    add doc comments

--- consumer.py
+++ consumer.py
@@ -5,13 +5,20 @@ from pyspark.sql import SparkSession
 
 
 def main():
+    """
+    This function for writing kafka messages from topic to files with selected format
+    :return:
+    """
     spark_app_name = "kafka_consumer"
     kafka_address = "172.18.0.2:6667"
     kafka_topic = "test"
     dir_for_checkpoint = "checkpoint"
 
     try:
+        # read first argv as output dir for result
         output_dir = sys.argv[1]
+
+        # read second argv as output format for result
         output_format = sys.argv[2]
         if output_format not in ("csv", "parquet"):
             exit("ERROR: Output format can be only csv or parquet")
@@ -20,12 +27,15 @@ def main():
             "ERROR: Enter an output directory as a first argument "
             "and output format as a second"
         )
-
+    # get dir name for checkpoint
     checkpoint_location = os.path.join(output_dir, dir_for_checkpoint)
+
+    # get dir name for output files
     output_path = os.path.join(output_dir, output_format)
 
     spark_session = SparkSession.builder.appName(spark_app_name).getOrCreate()
 
+    # read kafka topic with spark stream
     lines = (
         spark_session.readStream.format("kafka")
         .option("kafka.bootstrap.servers", kafka_address)
@@ -34,6 +44,7 @@ def main():
         .selectExpr("CAST(value AS STRING)")
     )
 
+    # write spark stream to files
     query = (
         lines.writeStream.outputMode("append")
         .format(output_format)
