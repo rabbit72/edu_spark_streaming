ade02c64b50d5572abedef903d1f09af182e9b2e
Daniil Shadrin <rabbit72rus@gmail.com>
2019-02-22 13:38:38 +0300

    add option for output (hdfs or elasticsearch)

--- consumer.py
+++ consumer.py
@@ -6,32 +6,33 @@ from pyspark.sql import SparkSession
 
 def main():
     """
-    This function for writing kafka messages from topic to files with selected format
+    This function for writing kafka messages from topic to files or to elasticsearch
     :return:
     """
     spark_app_name = "kafka_consumer"
     kafka_address = "172.18.0.2:6667"
     kafka_topic = "test"
     dir_for_checkpoint = "checkpoint"
+    es_nodes = "localhost"
+    es_index = "test"
+    es_document_type = "message"
 
     try:
-        # read first argv as output dir for result
+        # read first argv as work dir for checkpoint or result
         output_dir = sys.argv[1]
 
         # read second argv as output format for result
-        output_format = sys.argv[2]
-        if output_format not in ("csv", "parquet"):
-            exit("ERROR: Output format can be only csv or parquet")
+        output = sys.argv[2]
+        if output not in ("hdfs", "es"):
+            exit("ERROR: Output can be only hdfs or es")
     except IndexError:
         exit(
             "ERROR: Enter an output directory as a first argument "
-            "and output format as a second"
+            "and output as a second"
         )
+
     # get dir name for checkpoint
     checkpoint_location = os.path.join(output_dir, dir_for_checkpoint)
-
-    # get dir name for output files
-    output_path = os.path.join(output_dir, output_format)
 
     spark_session = SparkSession.builder.appName(spark_app_name).getOrCreate()
 
@@ -44,14 +45,26 @@ def main():
         .selectExpr("CAST(value AS STRING)")
     )
 
-    # write spark stream to files
-    query = (
-        lines.writeStream.outputMode("append")
-        .format(output_format)
-        .option("checkpointLocation", checkpoint_location)
-        .option("path", output_path)
-        .start()
+    query = lines.writeStream.outputMode("append").option(
+        "checkpointLocation", checkpoint_location
     )
+
+    if output == "hdfs":
+
+        output_format = "csv"
+        # get dir name for output files
+        output_path = os.path.join(output_dir, output_format)
+
+        # write spark stream to files
+        query = query.format(output_format).option("path", output_path).start()
+    elif output == "es":
+        # write spark stream to elasticsearch
+        query = (
+            query.format("org.elasticsearch.spark.sql")
+            .option("es.resource", "{0}/{1}".format(es_index, es_document_type))
+            .option("es.nodes", es_nodes)
+            .start()
+        )
 
     query.awaitTermination()
 
